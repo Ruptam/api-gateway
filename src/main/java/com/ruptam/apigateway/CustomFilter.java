35a3df959945e8b34ce9a9ddf117ce678d558d24
Ruptam <ruptam25@outlook.com>
2022-02-17 15:27:02 +0530

    added implementation for post fiter

--- src/main/java/com/ruptam/apigateway/CustomFilter.java
+++ src/main/java/com/ruptam/apigateway/CustomFilter.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.server.ServerWebExchange;
 
 import reactor.core.publisher.Mono;
@@ -20,11 +21,20 @@ public class CustomFilter implements GlobalFilter {
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         ServerHttpRequest request = exchange.getRequest();
         String authorizationHeader = request.getHeaders().getFirst("Authorization");
+
+        //if there is any code need to be executed for any particular service then use below if with url pattern
+        // if (request.getURI().toString().contains("/api/student")) {
+
+        // }
         log.info("Authorization Header ==> " + authorizationHeader);
         if (authorizationHeader.isEmpty()) {
             throw new RuntimeException("No Authorization header found");
         }
-        return chain.filter(exchange);
+        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+            ServerHttpResponse response = exchange.getResponse();
+            log.info("Post filter after executing the request and before sending the response to gateway => "
+                 + response.getStatusCode());
+        }));
     }
     
 }
